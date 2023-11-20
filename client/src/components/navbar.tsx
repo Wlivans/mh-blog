1541d5ad47e98e6edda22a88636f1e9006812fa8
meihao <kyu158233@gmail.com>
2023-11-20 16:04:26 +0800

    ✨ feat: i18n,alias,navbar

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,21 +1,57 @@
 import { Link } from "react-router-dom";
-import logo from "../imgs/logo.png";
-import { Search } from "lucide-react";
+import logo from "../images/logo.png";
+import { FileEdit, Search } from "lucide-react";
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import { cn } from "@/libs/utils";
 
 const Navbar = () => {
+	const { t } = useTranslation();
+
+	const [searchBoxVisibility, setSearchBoxVisibility] = useState(false);
+
+	const handleShowSearchBox = () => {
+		setSearchBoxVisibility((val) => !val);
+	};
+
 	return (
 		<nav className="navbar">
 			<Link to="/" className="w-10 flex-none">
 				<img src={logo} className="w-full cursor-pointer" alt="" />
 			</Link>
 
-			<div className="absolute left-0 top-full mt-0 w-full border-b border-grey bg-white px-[5vw] py-4 md:relative md:inset-0 md:block md:w-auto md:border-0 md:p-0">
+			<div
+				className={cn(
+					"absolute left-0 top-full mt-0 w-full border border-grey bg-white px-[5vw] py-4 duration-100 md:relative md:inset-0 md:block md:w-auto md:border-0 md:p-0",
+					searchBoxVisibility ? "block" : "hidden",
+				)}>
 				<input
 					type="text"
-					placeholder="Search"
+					placeholder={t("search")}
 					className="w-full rounded-full bg-grey p-4 pl-6 pr-[12%] placeholder:text-dark-grey md:w-auto md:pl-12 md:pr-6"
 				/>
 				<Search className="absolute right-[10%] top-1/2 h-6 w-6 -translate-y-3 text-2xl text-dark-grey md:pointer-events-none md:left-5" />
+			</div>
+
+			<div className="ml-auto flex items-center gap-3 md:gap-6">
+				<button
+					onClick={handleShowSearchBox}
+					className="md:hiddeen flex h-12 w-12 items-center justify-center rounded-full bg-grey">
+					<Search className="h-6 w-6 text-2xl" />
+				</button>
+
+				<Link to="/editor" className="link hidden gap-2 rounded md:flex">
+					<FileEdit />
+					<p>{t("write")}</p>
+				</Link>
+
+				<Link to="/signin" className="btn-dark py-2">
+					{t("user.signIn")}
+				</Link>
+
+				<Link to="/signin" className="btn-light hidden py-2 md:block">
+					{t("user.signUp")}
+				</Link>
 			</div>
 		</nav>
 	);
---
+++ client/src/locales/resources.ts
@@ -0,0 +1,10 @@
+import en from "./en-US.json";
+import zh from "./zh-CN.json";
+
+const resources = {
+	"en-US": { translation: en },
+	"zh-CN": { translation: zh },
+	zh: { translation: zh },
+};
+
+export default resources;
